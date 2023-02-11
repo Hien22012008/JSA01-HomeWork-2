80c7a006246db33b8ff7548d589583a7cb9c5e95
Hien22012008 <xuanhiennguyen34@gmail.com>
2023-02-11 16:03:33 +0700

    HomeWork

---
+++ data.js
@@ -0,0 +1,12 @@
+myArr = [9, 7, 9, 0, 7, 8, 387, 123, 456]
+console.log(myArr)
+
+for (var myArr = 0; myArr <= 0; myArr++) {
+    if (myArr % 2 || myArr == 0) {
+        console.log(0, 8, 456)
+    } else {
+        if (myArr % !2) {
+            console.log(7, 9, 123, 387)
+        }
+    }
+}
